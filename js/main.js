161ff58f0e52c6ffe7498ba58cd0d65819ce7460
Andrés Nicolás Rubio <87342325+andresnrubio@users.noreply.github.com>
2021-10-05 23:28:46 -0300

    json

--- js/main.js
+++ js/main.js
@@ -32,7 +32,7 @@ function respuestaStart() {
 console.log(playerOne);
 
 function showAndSelect() {
-  fetch("heroes.json",{
+  fetch("./heroes.json",{
          headers : { 
         'Content-Type': 'application/json',
         'Accept': 'application/json'
